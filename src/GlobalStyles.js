eb92e84c7c4ddc0d9c51bb1f50260f7159c31544
Valeria Rodionova <valeriya-off@yandex.ru>
2021-06-22 00:56:29 +0300

    start of the project

---
+++ src/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    overflow-x: hidden;
+  }
+
+  li {
+    list-style: none;
+  }
+`
